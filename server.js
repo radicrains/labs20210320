a1844c5230f4f9e66f9a26ab8246625a4b01051a
Juliana <juliana.mohamedali@gmail.com>
2021-03-20 11:58:30 +0800

    completed member task part 1

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// app.use(express.urlencoded({extended:false}));
+
+
+
+app.get('/test', (req, res)=>{
+	res.send('Hello World');
+});
+
+
+app.listen(port, ()=> {
+    console.log('Missions to Mars running on port: ', port);
+});
